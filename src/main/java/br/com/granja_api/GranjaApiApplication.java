273a6be50616532abff65e26d521e25aea51b73f
Rodrigo Valença <rodrigovalenca@MacBook-Air-de-Rodrigo.local>
2026-08-09 22:10:10 -0300

    US-001: Inicializar projeto Spring Boot (Java 17, Maven) + Docker/Docker Compose

---
+++ src/main/java/br/com/granja_api/GranjaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.granja_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranjaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/granja_api/GranjaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.granja_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
